79680591319164485569d76dc0d37a021a35452b
Leonardo <leo.abreudp@gmail.com>
2023-04-26 20:42:46 +0100

    sidebar styling

--- resources/views/components/sidebar/index.blade.php
+++ resources/views/components/sidebar/index.blade.php
@@ -3,12 +3,14 @@
     <div class="py-4">
       <a href="https://www.booking.com/index.en-gb.html?aid=397594&label=gog235jc-1DCAEoggI46AdIM1gDaLsBiAEBmAEJuAEXyAEM2AED6AEBiAIBqAIDuALM-qWiBsACAdICJGZjZTFmYzQ5LTRiODItNDNmMi1hMjg5LWY5MGQ0OGFhZDIyNtgCBOACAQ&sid=976bbdb79e60801dd0ec93ca914b5599&keep_landing=1&sb_price_type=total&"><x-basics.h1 :text="'Tripmaster'"/></a>
     </div>
-    <div class="h-0.5 w-full bg-white"></div>
+    <div class="h-0.5 w-full dark:bg-white bg-black"></div>
   </div>
   <ul class="px-2 dark:text-white">
     <x-sidebar.li :href="route('home')" :name="'Home'" :current="$current == 'home'"/>
     <x-sidebar.li :href="route('list.stays')" :name="'Stays'" :current="$current == 'stays.index'"/>
-    <div class="h-0.5 w-full bg-white"></div>
+    <div class="py-4">
+      <div class="h-0.5 w-full dark:bg-white bg-black"></div>
+    </div>
     <x-sidebar.li :href="route('my.projects')" :name="'Projects'" :current="$current == 'projects.list' || $current == 'projects.create'"/>
     <x-sidebar.li :href="route('toggle-theme')" :name="'Darkmode'" :current="false"/>
   </ul>
