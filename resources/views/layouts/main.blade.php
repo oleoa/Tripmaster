32aee39006a967666d2bcd57471659ae2d2ceb8e
Leonardo <leo.abreudp@gmail.com>
2023-04-26 10:39:42 +0100

    body layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,13 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @vite('resources/css/app.css')
   <title>{{ $title }}</title>
 </head>
 <body>
-  <nav>
+  <nav class='h-screen w-24 bg-blue-500 absolute left-0'>
     <x-sidebar/>
   </nav>
-  <main>
+  <main class='pl-28'>
     @yield('body')
   </main>
 </body>
