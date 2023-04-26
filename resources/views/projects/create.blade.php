99a1597eb3bfd34c67f6b6d3b89af71412f22fd0
Leonardo <leo.abreudp@gmail.com>
2023-04-26 18:51:21 +0100

    create project login style

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,18 +3,15 @@
 <div class="w-full flex justify-center items-center p-4">
   <form action="{{route('create.project')}}" method="POST" class="grid grid-cols-2 gap-4">
     @csrf
-    <x-basics.h1 :text="'Title'"/>
+    <x-basics.h2 :text="'Title'"/>
     <x-form.input :type="'text'" :name="'title'"/>
-    
-    <x-basics.h1 :text="'Country'"/>
-    <x-form.input :type="'text'" :name="'country'"/>
-    
-    <x-basics.h1 :text="'Date'"/>
-    <x-form.input :type="'date'" :name="'date'"/>
-    
-    <x-basics.h1 :text="'How many people will join you in this adventure?'"/>
+    <x-basics.h2 :text="'Country'"/>
+    <x-form.input :type="'text'" :name="'country'"/>    
+    <x-basics.h2 :text="'Date'"/>
+    <x-form.input :type="'date'" :name="'date'"/>    
+    <x-basics.h2 :text="'How many people will join you in this adventure?'"/>
     <x-form.input :type="'integer'" :name="'headcount'"/>
-
+      
     <input type="submit" value="Create">
   </form>
 </div>
