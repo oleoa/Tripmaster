01effb27df28d7e0d4e5f8b8cdeb41936792ce2f
Leonardo <leo.abreudp@gmail.com>
2023-05-08 13:00:37 +0100

    starting account page

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+  public function index()
+  {
+    $this->data->title('Account');
+    return $this->view('account');
+  }
 }
---
+++ resources/views/account.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.main')
+@section('body')
+  <h1>Account</h1>
+@endsection
