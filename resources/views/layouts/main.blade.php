dbd103cbe68467205a7c6b96b9d606ecc0fbb5fd
Leonardo <leo.abreudp@gmail.com>
2023-05-16 09:10:09 +0100

    head to component

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  @vite('resources/css/app.css')
-  <title>@lang($title)</title>
-</head>
+<x-head :title="$title"/>
 <body class='{{ $theme }}'>
   <nav class='h-full w-48 border-r-2 border-turquoise-200 dark:border-slate-300 fixed left-0'>
     <x-sidebar.index :current="$current" :logged="Auth::check()" :inverseTheme="$inverseTheme"/>
