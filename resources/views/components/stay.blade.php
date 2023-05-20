e266b8802d807f38c3d07f4006b52913ebf8fe25
Leonardo <leo.abreudp@gmail.com>
2023-05-20 21:00:18 +0100

    stays list

--- resources/views/components/stay.blade.php
+++ resources/views/components/stay.blade.php
@@ -1,8 +1,4 @@
-<div class="dark:bg-slate-700 bg-turquoise-100 text-white rounded p-4 grid grid-cols-3 w-full">
-  <div class="w-full h-32 col-span-2">
-    <h1>{{$stay['title']}}</h1>
-  </div>
-  <div class="flex flex-col justify-between">
-    <h1>{{$stay['description']}}</h1>
-  </div>
+<div class="dark:bg-slate-700 bg-turquoise-100 text-white rounded p-4 grid grid-rows-2 w-full">
+  <h1 class="py-2">{{$stay['title']}}</h1>
+  <p>{{$stay['description']}}</p>
 </div>
