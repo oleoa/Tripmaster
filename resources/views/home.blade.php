644444cd3df3eed3d1c82140550adb510dd97e18
Leonardo <leo.abreudp@gmail.com>
2023-05-29 21:10:39 +0100

    home page haha

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.main')
 @section('body')
-  <h1>Home Page</h1>
+  <div class="flex flex-col items-center justify-center p-6 space-y-6">
+    <h1>Home Page</h1>
+    <img src="{{asset('images/aeroport.jpg')}}" alt="Aeroport" class="rounded w-1/2">
+  </div>
 @endsection
