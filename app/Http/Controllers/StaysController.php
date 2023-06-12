22b62570c7e3247a96177ef206f9cc65b57fb720
Leonardo <leo.abreudp@gmail.com>
2023-06-12 20:37:41 +0100

    showing stays

--- app/Http/Controllers/StaysController.php
+++ app/Http/Controllers/StaysController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\Stays_Images;
+use App\Models\Stays;
 use App\Models\User;
 
 class StaysController extends Controller
@@ -21,11 +23,15 @@ class StaysController extends Controller
     if(!$project)
       return redirect()->route('my.creator.project');
       
-    $this->data->set("p", $project);
-
     $this->data->set('country', $project->country);
+    
+    $stays = Stays::where("country", $project->country)->get();
+    for($i = 0; $i < count($stays); $i++)
+    {
+      $stays[$i]->image = Stays_Images::where("stay", $stays[$i]->id)->first()->image_path ?? false;
+    }
+    $this->data->set('stays', $stays);
 
     return $this->view('stays.index');
   }
-
 }
--- resources/views/stays/index.blade.php
+++ resources/views/stays/index.blade.php
@@ -2,5 +2,20 @@
 @section('body')
   <div class="p-6 grid">
     <h1 class="text-center">Stays for {{$country}}</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
+      @foreach ($stays as $stay)
+        <div class="bg-slate-700 rounded-lg shadow-lg overflow-hidden">
+          <img class="w-full h-56 object-cover object-center" src="{{asset('storage/stays/'.$stay->image)}}" alt="{{$stay->title}}">
+          <div class="p-4">
+            <h1 class="text-2xl font-semibold">{{$stay->title}}</h1>
+            <p class="mt-2 text-white">{{$stay->description}}</p>
+            <div class="flex justify-between items-center mt-4">
+              <a href="{{route('main', $stay->id)}}" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded">View</a>
+              <span class="font-semibold text-xl text-white">{{$stay->price}}€</span>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
   </div>
 @endsection
