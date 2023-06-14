4c48cc6c62468d45f85185d78786719c36e56434
Leonardo <leo.abreudp@gmail.com>
2023-06-14 16:10:12 +0100

    fixing stays table

--- resources/views/my/stays/list.blade.php
+++ resources/views/my/stays/list.blade.php
@@ -13,23 +13,23 @@
     </div>
 
     <div>
-      <div class="grid grid-cols-6 w-full bg-slate-600 p-4 rounded-t">
+      <div class="grid grid-cols-7 w-full bg-slate-600 p-4 rounded-t">
         <h6>Name</h6>
         <h6>Status</h6>
         <h6>Capacity</h6>
         <h6>Price</h6>
         <h6>City</h6>
-        <h6 class="text-end">Actions</h6>
+        <h6 class="text-end col-span-2">Actions</h6>
       </div>
       <div class="grid grid-cols-1">
         @foreach ($stays as $stay)
-          <div class="dark:bg-slate-700 bg-turquoise-100 [&>h3]:flex [&>h3]:items-center text-white p-4 grid grid-cols-6 w-full @if($loop->last) rounded-b @else border-b-2 @endif">
-            <h3 class="pr-8">{{$stay['title']}}</h3>
-            <h3>{{$stay['status']}}</h3>
-            <h3>{{$stay['capacity']}}</h3>
-            <h3>{{$stay['price']}}</h3>
-            <h3>{{$stay['city']}}</h3>
-            <div class="flex items-center justify-end space-x-4">
+          <div class="dark:bg-slate-700 bg-turquoise-100 [&>h4]:flex [&>h4]:items-center text-white p-4 grid grid-cols-7 w-full @if($loop->last) rounded-b @else border-b-2 @endif">
+            <h4 class="pr-8">{{$stay['title']}}</h4>
+            <h4>{{$stay['status']}}</h4>
+            <h4>{{$stay['capacity']}}</h4>
+            <h4>{{$stay['price']}}€</h4>
+            <h4>{{$stay['city']}}</h4>
+            <div class="flex items-center justify-end space-x-4 col-span-2">
               <a class="btn" href="{{route("show.stay", ['id' => $stay['id']])}}">@lang('View')</a>
               <a class="btn-okay" href="{{route('my.editor.stay', ['id' => $stay['id']])}}">@lang('Edit')</a>
               @if ($stay['status'] == 'available')
@@ -45,6 +45,7 @@
     </div>
     
   @else
+
     <div class="flex items-center justify-center relative">
       <div class="py-6">
         <h1>@lang('You have no stays')</h1>
@@ -53,6 +54,7 @@
         <a href="{{route('my.creator.stay')}}" class="btn">@lang('Create Stay')</a>
       </div>
     </div>
+    
   @endif
 </main>
 @endsection
