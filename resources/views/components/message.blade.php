3d58ad17b76ccf8f9bb7c3158989b1ad39533901
Leonardo <leo.abreudp@gmail.com>
2023-06-17 12:06:52 +0100

    message dark background

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,6 +1,6 @@
 @if($message)
   <div id="opc" class="fixed top-0 right-0 w-full bg-slate-700/50 h-full">
-    <dialog class="bg-white rounded my-4 w-1/3" open>
+    <dialog class="bg-slate-950 rounded my-4 w-1/3" open>
       <h1 class="message {{$type}}">@lang($title):</h1>
       <p class="text-xl py-4 message {{$type}}"><strong><code>@lang($message)<code><strong></p>
       <form method="dialog" class="text-end">
