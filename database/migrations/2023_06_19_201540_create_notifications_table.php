53d81dec1dd443b058ab92339b5fce4f3f6207d8
Leonardo <leo.abreudp@gmail.com>
2023-06-19 21:19:05 +0100

    notifications table

---
+++ database/migrations/2023_06_19_201540_create_notifications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('notifications', function (Blueprint $table) {
+      $table->id();
+      $table->string('title');
+      $table->string('body');
+      $table->date('date');
+      $table->boolean('seen')->default(false);
+      $table->unsignedBigInteger('user');
+      $table->foreign('user')->references('id')->on('users');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('notifications');
+  }
+};
