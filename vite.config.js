154535117b2ed86d618bcf380fc42e77a312261d
Leonardo <leo.abreudp@gmail.com>
2023-06-30 19:48:31 +0100

    vite config for server

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+              'resources/css/app.css',
+              'resources/css/carousel.css',
+              'resources/css/rating.css',
+              'resources/css/toggle.css',
+              'resources/js/app.js',
+              'resources/js/carousel.js',
+              'resources/js/calendar.js',
+              'resources/js/rating.js',
+              'resources/js/selectAll.js'
+            ],
             refresh: true,
         }),
     ],
