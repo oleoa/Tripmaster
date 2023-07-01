927d9b9ac3f14f70410369afb7e4c0b1e022440d
Leonardo <leo.abreudp@gmail.com>
2023-07-01 18:15:38 +0100

    signup cs

--- resources/views/sign/up.blade.php
+++ resources/views/sign/up.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.main')
 @section('body')
-  <div class="w-full flex flex-col justify-center items-center">
-    <div class="py-12">
+  <div class="w-full flex flex-col justify-center items-center space-y-4">
+    <div class="py-8">
       <h1>@lang('Sign up')</h1>
     </div>
-    <form action="{{route('sign.ing-up')}}" method="post" class="grid grid-cols-1 gap-4 w-1/3">
+    <form action="{{route('sign.ing-up')}}" method="post" class="grid grid-cols-1 gap-4 w-full lg:w-1/3">
       @csrf
       <label for="name"><h2>@lang('Name')</h2></label>
       <input type="text" name="name" id="name" value="{{old('name')}}"/>
@@ -16,7 +16,7 @@
       <input type="password" name="password_confirmation" id="password_confirmation" minlength="{{$password_min_length}}" maxlength="{{$password_max_length}}"/>
       <button type="submit" class="btn good">@lang('Sign up')</button>
     </form>
-    <div class="p-4">
+    <div class="p-0 lg:p-4">
       <h2 class="text-2xl">@lang('Have an account?') <a class="text-blue-500" href="{{route('sign.in')}}">@lang('Sign in')</a>!</h2>
     </div>
   </div>
